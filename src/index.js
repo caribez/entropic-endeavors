231ca48e74b99359198e9808afd7ce40e3929f2d
caribez <41511020+caribez@users.noreply.github.com>
2023-12-31 14:39:35 +0100

    Create index.js
    An initial test version to get server started.

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// Import required modules
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Create an instance of Express
+const app = express();
+
+// Middleware to parse incoming JSON data
+app.use(bodyParser.json());
+
+// Endpoint to handle GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to the interactive musical performance server!');
+});
+
+// Endpoint to handle POST requests (for audience button presses)
+app.post('/buttonPress', (req, res) => {
+  const { buttonId } = req.body;
+  // Process the button press data here, update variables, etc.
+  console.log(`Button ${buttonId} pressed`);
+  res.status(200).send('Button press received successfully!');
+});
+
+// Set the port for the server to listen on
+const PORT = 3000; // default to 3000
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
