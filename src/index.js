3a20fb0dd3e9926cd12dba0964a5ef0807763990
caribez <41511020+caribez@users.noreply.github.com>
2024-01-02 23:04:39 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,14 @@ const app = express();
 // Middleware to parse incoming JSON data
 app.use(bodyParser.json());
 
-let value = 0;
+const direction = { 
+  'x': 0, 
+  'y': 0,
+  'votes': 0,
+  'xNorm': 0,
+  'yNorm': 0
+};
+
 
 // Serve static files (HTML, CSS, JS) from a directory named 'public'
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -22,7 +29,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/reset', (req, res) => {
-  value = 0;
+  //value = 0;
   res.sendStatus(200); // Sends a response with HTTP status code 200 (OK)
 });
 
@@ -38,11 +45,24 @@ app.get('/messages', (req, res) => {
 
 // Endpoint to handle POST requests (for audience button presses)
 app.post('/buttonPress', (req, res) => {
-  const { buttonId } = req.body;
-  value += buttonId;
-  // Process the button press data here, update variables, etc.
-  console.log(`Button ${buttonId} pressed`);
-  console.log(`value: ${value}`);
+  const button = req.body;
+  console.log(button.buttonId + " button pressed by a user")
+  direction.x += button.x;
+  direction.y += button.y;
+  direction.votes++;
+  
+  const averageVector = {
+  x: direction.x / direction.votes,
+  y: direction.y / direction.votes,
+};
+
+  const length = Math.sqrt(averageVector.x ** 2 + averageVector.y ** 2);
+  
+  direction.xNorm = averageVector.x / length;
+  direction.yNorm = averageVector.y / length;
+
+  console.log(`Direction is (${direction.xNorm}, ${direction.yNorm})`);
+  //console.log(`value: ${value}`);
   res.status(200).send('Button press received successfully!');
 });
 
